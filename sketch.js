e01e47545cc3f99342658bc73041f67658a7c23c
Jeremy-Kwok <87737826+Jeremy-Kwok@users.noreply.github.com>
2022-06-02 23:44:00 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,20 +1,21 @@
+let center_x = 50;
+let center_y= 50;
+let lefteye_x= 35;
+let lefteye_y= 40;
+let righteye_x= 65;
+let righteye_y= 40;
+   
 function setup() {
   createCanvas(100, 100);
-  var center_x,center_y,lefteye_x, lefteye_y, righteye_x, righteye_y;
-  let center_x= int(50);
-  let center_y= int(50);
-  let lefteye_x= int(35);
-  let lefteye_y= int(40);
-  let righteye_x= int(65);
-  let righteye_y= int(40);
+  //var center_x,center_y,lefteye_x, lefteye_y, righteye_x, righteye_y;
 }
 
 function draw() {
-  fill(int(255), int(255), int(0));
-  circle(center_x,center_y, int(50));
-  fill(int(0),int(0),int(255));
-  circle(lefteye_x,lefteye_y, int(10));
-  circle(righteye_x,righteye_y, int(10));
-  fill(int(148), int(0), int(211));
-  triangle(int(center_x-5), int(center_y-5), int(center_x+5), int(center_y-5), center_x, int(center_y+20));
+  fill(255, 255, 0);
+  circle(center_x,center_y, 50);
+  fill(0,0,255);
+  circle(lefteye_x,lefteye_y, 10);
+  circle(righteye_x,righteye_y, 10);
+  fill(148, 0, 211);
+  triangle(center_x-5, center_y-5, center_x+5, center_y-5, center_x, center_y+20);
 }
