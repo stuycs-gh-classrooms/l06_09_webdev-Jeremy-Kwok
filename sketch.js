7a54a0a1cd65c1c1438dd437b1769a96ba84e06c
Jeremy-Kwok <87737826+Jeremy-Kwok@users.noreply.github.com>
2022-06-02 23:54:23 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,16 +6,43 @@ let righteye_x= 65;
 let righteye_y= 40;
    
 function setup() {
-  createCanvas(100, 100);
+  createCanvas(400, 400);
   //var center_x,center_y,lefteye_x, lefteye_y, righteye_x, righteye_y;
 }
 
 function draw() {
-  fill(255, 255, 0);
-  circle(center_x,center_y, 50);
-  fill(0,0,255);
-  circle(lefteye_x,lefteye_y, 10);
-  circle(righteye_x,righteye_y, 10);
-  fill(148, 0, 211);
-  triangle(center_x-5, center_y-5, center_x+5, center_y-5, center_x, center_y+20);
+  while (center_y<400){
+    while (center_x < 400){
+      fill(255, 255, 0);
+      circle(center_x,center_y, 50);
+      fill(0,0,255);
+      circle(lefteye_x,lefteye_y, 10);
+      circle(righteye_x,righteye_y, 10);
+      fill(148, 0, 211);
+      triangle(center_x-5, center_y-5, center_x+5, center_y-5, center_x, center_y+20);
+      
+      center_x += 100;
+    }
+  }
 }
+
+//   while (center_y < 400) {
+//     while (center_x < 400) {
+//       avatar(center_x, center_y);
+//       center_x += 100;
+//     } //end of x while
+//     center_x = 50;
+//     center_y += 100;
+//   } //end of y while
+// } //end of setup
+
+
+// void avatar(int center_x, int center_y) {
+//   fill(255, 255, 0);
+//   circle(center_x,center_y, 100);
+//   fill(0,0,255);
+//   circle((center_x-15),(center_y-10), 20);
+//   circle((center_x+15),(center_y-10), 20);
+//   fill(148, 0, 211);
+//   triangle((center_x-10), (center_y), (center_x+10), (center_y), center_x, (center_y+40));
+// }
